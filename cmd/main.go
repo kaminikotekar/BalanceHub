d21af3df007a5271928baaf592b27a1bc79d6a8e
Kamini Kotekar <kaminikotekar@gmail.com>
2023-08-08 19:49:41 +1000

    Create main.go

---
+++ cmd/main.go
@@ -0,0 +1,151 @@
+package main
+import ("os"
+		"log"
+		"bufio"
+        "fmt"
+        "net"
+		"net/http"
+		"net/url"
+		"bytes"
+		"github.com/kaminikotekar/BalanceHub/pkg/Config"
+		"github.com/kaminikotekar/BalanceHub/pkg/Connection"
+)
+
+
+func main() {
+	log.Println("Starting HTTP server...")
+	config, err := Config.GetConfiguration("config.yaml")
+	
+	loadBalancer := config.LoadBalancer
+	remoteServers := config.OriginalServers
+	connectionLoad := &Connection.Connections{
+		ActiveConnections: make(map[Config.Server]int),
+	}
+	connectionLoad.InitializeLoadServers(remoteServers)
+
+	fmt.Println("loaad: ", connectionLoad)
+	fmt.Println("Server List ", remoteServers)
+
+	reverseProxy, err := net.Listen("tcp", loadBalancer.Ipaddress+":"+loadBalancer.Port)
+	if err != nil {
+		log.Printf("Error listening: %s", err.Error())
+		os.Exit(1)
+	}
+	
+	defer reverseProxy.Close()
+	log.Println("Listening on %s:%s",loadBalancer.Ipaddress, loadBalancer.Port)
+	for {
+		conn, err := reverseProxy.Accept()
+		if err != nil {
+			log.Println("Error accepting: %s", err.Error())
+			continue
+		}
+		fmt.Println("Received connection from ", conn)
+		// read data from connection
+		go handleConnection(conn, connectionLoad)
+	}
+}
+
+func updateRequestParms(req *http.Request, uri string) error{
+	newURL, err := url.Parse("http://"+uri)
+	if err != nil {
+		fmt.Println("Error parsing new URL:", err)
+		return  err
+	}
+	req.Host = newURL.Host
+	req.URL.Host = newURL.Host
+	req.URL.Scheme = newURL.Scheme
+	req.RequestURI = ""
+
+	return nil
+}
+
+func writeRequestToBytes(req *http.Request) ([]byte, error){
+	var buf bytes.Buffer
+	bufPointer := &buf
+	err := req.Write(bufPointer)
+	if err!=nil {
+		return nil, err
+	}
+	bytes := bufPointer.Bytes()
+	return bytes, nil
+}
+
+func writeResponseToBytes(res *http.Response) ([]byte, error){
+	var buf bytes.Buffer
+	bufPointer := &buf
+	err := res.Write(bufPointer)
+	if err!=nil {
+		return nil, err
+	}
+	bytes := bufPointer.Bytes()
+	return bytes, nil
+}
+
+func performReverseProxy(req *http.Request, uri string) ([]byte, error){
+
+	remoteServerConnect, err := net.Dial("tcp", uri)
+	if err != nil {
+		fmt.Println("Error connecting to original server: ", err)
+		return nil, err
+	}
+
+	updateRequestParms(req, uri)
+	bytesToSend , err := writeRequestToBytes(req) 
+	if err != nil {
+		return nil, err
+	}
+	_, err = remoteServerConnect.Write(bytesToSend)
+
+	// Reader 2
+	reader := bufio.NewReader(remoteServerConnect)
+	response, err := http.ReadResponse(reader, nil)
+	defer response.Body.Close()
+	fmt.Printf("type: %T \n", response)
+	fmt.Println("response: ", response.Body)
+	if err != nil {
+		// fmt.Println("Error reading response:", err2)
+		return nil, err
+	}
+	
+	fmt.Println("response to send back to client", response)
+
+	bytesToSend, err = writeResponseToBytes(response)
+	remoteServerConnect.Close()
+	return  bytesToSend, nil
+}
+
+func handleConnection(conn net.Conn, connectionLoad *Connection.Connections) {
+	
+	defer conn.Close()
+
+	// Create an HTTP request reader
+	reader := bufio.NewReader(conn)
+
+	// Read the HTTP request
+	request, err := http.ReadRequest(reader)
+
+	fmt.Println("request: ", request)
+	fmt.Printf("type: %T \n", request)
+	if err != nil {
+		fmt.Println("Error reading HTTP request:", err)
+		return
+	}
+
+	// Find Server with least active connection
+	fmt.Println("printing optimal server: ......")
+	server := connectionLoad.GetOptimalServer()
+	fmt.Println("Optimal Server: ", server)
+
+	response, err := performReverseProxy(request, server.Ipaddress + ":" + server.Port)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	conn.Write(response)
+
+}
+
+
+
